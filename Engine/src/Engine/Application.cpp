c5fc5f4ea3a48091cd5e52292077c135d8865e89
Gaston M. Pannuto <gastonpannuto@gmail.com>
2023-03-09 20:28:01 -0300

    Camera Movement

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -5,10 +5,6 @@
 #include "Renderer/Texture.h"
 #include "Renderer/VertexArray.h"
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include <iostream>
 
 int main()
@@ -72,21 +68,39 @@ int main()
         // Input
         processInput(window);
 
-        // rendering commands
+        // Rendering Commands
         glEnable(GL_DEPTH_TEST);
         glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // Render Loop
         texture1.Bind(0);
         texture2.Bind(1);
 
+		// Model
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
+        // View
         glm::mat4 view = glm::mat4(1.0f);
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0));
 
+    	// Projection
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        glm::vec3 cubePositions[] = {
+		    glm::vec3(0.0f,  0.0f,  0.0f),
+		    glm::vec3(2.0f,  5.0f, -15.0f),
+		    glm::vec3(-1.5f, -2.2f, -2.5f),
+		    glm::vec3(-3.8f, -2.0f, -12.3f),
+		    glm::vec3(2.4f, -0.4f, -3.5f),
+		    glm::vec3(-1.7f,  3.0f, -7.5f),
+		    glm::vec3(1.3f, -2.0f, -2.5f),
+		    glm::vec3(1.5f,  2.0f, -2.5f),
+		    glm::vec3(1.5f,  0.2f, -1.5f),
+		    glm::vec3(-1.3f,  1.0f, -1.5f)
+        };
 
         TestShader.Bind();
         TestShader.SetMat4("model", model);
@@ -94,8 +108,16 @@ int main()
         TestShader.SetMat4("projection", projection);
         VAO.Bind();
 
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            TestShader.SetMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // Check and call events and swap the buffers
         glfwSwapBuffers(window);
--- Engine/src/Engine/Application.h
+++ Engine/src/Engine/Application.h
@@ -2,9 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // Settings
 unsigned int Screen_Width = 1280;
 unsigned int Screen_Height = 720;
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+
+// Functions
+inline void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+inline void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    const float cameraSpeed = 0.05f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
 
 // Data
 float vertices1[] = {
@@ -85,15 +112,3 @@ unsigned int indices[] = {  // note that we start from 0!
     0, 1, 3,   // first triangle
     1, 2, 3    // second triangle
 };
-
-// Functions
-inline void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-inline void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
